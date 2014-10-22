af88634475d00d79197201488c0696c45cd6cbd4
Matt McPherrin <git@mcpherrin.ca>
2014-10-22 00:45:21 -0700

    Start adding some Json encoding for State.
    I used the ToJson instead of going via Encodable/Encoder because they're
    just totally annoying to do anything nonstandard.

--- src/api.rs
+++ src/api.rs
@@ -4,6 +4,10 @@
 //! Enums are used in place of strings when there is a fixed set of values.
 
 pub mod light {
+  use std::collections::TreeMap;
+  use serialize::json;
+  use serialize::json::ToJson;
+
   /// All except transitiontime are returned when you read the state on a hue.
   /// Hue flux bulbs don't have color info.  The bridge is buggy if you set
   /// conflicting color mode options.  Reachable is always true.
@@ -22,9 +26,67 @@ pub mod light {
     pub transitiontime: Option<u16>
   }
 
+  // Macros to the rescue for verbose code!
+  macro_rules! maybe_insert(
+    ($slf: ident, $field:ident, $json_name: expr, $object: ident) => (
+      if let Some($field) = $slf.$field {
+        $object.insert($json_name.to_string(), $field.to_json());
+      }
+    );
+    ($slf: ident, $field:ident, $object: ident) => (
+      maybe_insert!($slf, $field, stringify!($field), $object)
+    )
+  )
+  impl ToJson for State {
+    fn to_json(&self) -> json::Json {
+      let mut object = TreeMap::new();
+      maybe_insert!(self, on, object);
+      maybe_insert!(self, bri, object);
+      maybe_insert!(self, hue, object);
+      maybe_insert!(self, sat, object);
+      maybe_insert!(self, xy, object);
+      maybe_insert!(self, ct, object);
+      maybe_insert!(self, alert, object);
+      maybe_insert!(self, effect, object);
+      maybe_insert!(self, colormode, object);
+      maybe_insert!(self, reachable, object);
+      maybe_insert!(self, transitiontime, object);
+      json::Object(object)
+    }
+  }
+
   pub enum ColorMode { HueSat, CieXy, ColorTemperature }
+  impl ToJson for ColorMode {
+    fn to_json(&self) -> json::Json {
+      json::String(
+        match *self {
+          HueSat => "hs",
+          CieXy => "xy",
+          ColorTemperature => "ct"
+        }.to_string())
+    }
+  }
   pub enum Alert { NoAlert, Select, LSelect }
+  impl ToJson for Alert {
+    fn to_json(&self) -> json::Json {
+      json::String(
+        match *self {
+          NoAlert => "none",
+          Select => "select",
+          LSelect => "lselect"
+        }.to_string())
+    }
+  }
   pub enum Effect { NoEffect, ColorLoop }
+  impl ToJson for Effect {
+    fn to_json(&self) -> json::Json {
+      json::String(
+        match *self {
+          NoEffect => "none",
+          ColorLoop => "colorloop",
+        }.to_string())
+    }
+  }
 
   /// Returned from /api/<username>/lights/<id>
   pub struct Attributes {
@@ -38,4 +100,23 @@ pub mod light {
 
   /// Reserved for future use, apparently.
   pub struct PointSymbol;
+
+  #[test]
+  fn test_encode_state() {
+    //! A wholly incomplete test
+    let mut state = State{on: None, bri: None, hue: None, sat: None, xy: None, ct: None, alert: None, effect: None, colormode: None, reachable: None, transitiontime: None };
+    assert_eq!(state.to_json().to_string(), "{}".to_string());
+    state.on = Some(true);
+    assert_eq!(state.to_json().to_string(),
+               "{\"on\":true}".to_string());
+    state.on = Some(false);
+    assert_eq!(state.to_json().to_string(),
+               "{\"on\":false}".to_string());
+  }
+
+
+
 }
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(macro_rules)]
+
+extern crate serialize;
 pub mod api;
 
 #[test]
