688a23bfcdec3b64ca0f476b6ab84b21c9bd4c47
Matt McPherrin <git@mcpherrin.ca>
2014-10-25 14:31:42 -0700

    Basic on/off functionality works!

---
+++ examples/turn_off.rs
@@ -0,0 +1,11 @@
+//! An example of using the raw types and bridge API
+extern crate hue;
+extern crate serialize;
+use serialize::json::ToJson;
+
+fn main() {
+  let mut bridge = hue::bridge::Bridge::new("192.168.1.10".to_string(), "newdeveloper".to_string());
+  let mut state = hue::api::light::State{on: None, bri: None, hue: None, sat: None, xy: None, ct: None, alert: None, effect: None, colormode: None, reachable: None, transitiontime: None };
+  state.on = Some(false);
+  let data: Option<String> = bridge.put("lights/1/state", Some(state.to_json()));
+}
--- src/bridge.rs
+++ src/bridge.rs
@@ -19,29 +19,29 @@ impl Bridge {
   fn username(&self) -> &str { self.username.as_slice() }
 
   pub fn get<T: FromJson>(&mut self, path: &str) -> Option<T> {
-    let url = format!("http://{:s}/api/{:s}/{:s}", self.host(), self.username(), path);
-    let resp = self.handle.get(url).exec();
-    match resp {
-      Ok(resp) => {
-        if resp.get_code() != 200 { return None };
-        super::json_helper::FromJson::from_json(&resp.get_body().to_json())
-      },
-      _ => None
-    }
+    self.request(curl::http::handle::Get, path, None)
   }
 
-  /// Do a POST request to the bridge API.  Don't include the /api/username portion.
-  pub fn post<T: FromJson>(&mut self, path: &str, body: Option<Json>) -> Option<T> {
-    let url = format!("http://{:s}/api/{:s}/{:s}", self.host(), self.username(), path);
-    let bs = if body.is_some() {
-      body.to_string()
-    } else {
-      "".to_string()
-    };
-    let resp = self.handle.post(url, bs.as_slice()).exec();
+  pub fn put<T: FromJson>(&mut self, path: &str, body: Option<Json>) -> Option<T> {
+    self.request(curl::http::handle::Put, path, body)
+  }
+
+  /// Do a request to the bridge API.  Don't include the /api/username portion in path.
+  pub fn request<T: FromJson>(&mut self, method: curl::http::handle::Method, path: &str, body: Option<Json>) -> Option<T> {
+    let uri = format!("http://{:s}/api/{:s}/{:s}", self.host(), self.username(), path);
+    let mut request = curl::http::handle::Request::new(&mut self.handle, method).uri(uri);
+    let mut bs;
+    if let Some(body) = body {
+      bs = body.to_string();
+      request = request.body(&bs);
+    }
+    let resp = request.exec();
     match resp {
       Ok(resp) => {
         if resp.get_code() != 200 { return None };
+        // debug print:
+        println!("{}", ::std::str::from_utf8(resp.get_body()));
+        //
         super::json_helper::FromJson::from_json(&resp.get_body().to_json())
       },
       _ => None
