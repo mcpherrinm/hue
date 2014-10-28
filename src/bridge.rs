2965116f14411dc1e7a73df5e46c982c938e8e7c
Matt McPherrin <git@mcpherrin.ca>
2014-10-28 00:29:08 -0700

    Buggy URLs with two // are troublesome

--- src/bridge.rs
+++ src/bridge.rs
@@ -29,7 +29,8 @@ impl Bridge {
 
   /// Do a request to the bridge API.  Don't include the /api/username portion in path.
   pub fn request<T: FromJson>(&mut self, method: curl::http::handle::Method, path: &str, body: Option<Json>) -> Option<T> {
-    let uri = format!("http://{:s}/api/{:s}/{:s}", self.host(), self.username(), path);
+    let uri = format!("http://{:s}/api/{:s}{:s}", self.host(), self.username(), path);
+    println!("Url: {}", uri);
     let mut request = curl::http::handle::Request::new(&mut self.handle, method).uri(uri);
     let mut bs;
     if let Some(body) = body {
@@ -41,7 +42,7 @@ impl Bridge {
       Ok(resp) => {
         if resp.get_code() != 200 { return None };
         // debug print:
-        println!("{}", ::std::str::from_utf8(resp.get_body()));
+        println!("Response: {}", ::std::str::from_utf8(resp.get_body()));
         //
         super::json_helper::FromJson::from_json(&resp.get_body().to_json())
       },
